a555478e10e25fe906783105fcd452416bfa8755
cdisciascio <cecidisi@gmail.com>
2016-04-07 18:06:00 +0200

    Fixing save file bug

--- server/save.php
+++ server/save.php
@@ -20,7 +20,9 @@ if(!is_writable($output_dir)) {
 $filename = $_POST['filename'];
 $content = $_POST['content'];
 $file = fopen($output_dir.'/'.$filename, 'w') or die('Unable to open file!');
-fwrite($file, $content);
+$op = fwrite($file, $content);
 fclose($file);
+if($op === false)
+	die("Fwrite failed");
 echo $filename." saved succesfully";
 ?>
